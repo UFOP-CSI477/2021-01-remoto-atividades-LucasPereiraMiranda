93493d980f88ec82eb3bce7a6b9c9defc495d45e
= <=>
2021-11-03 21:55:07 -0300

    Atividade Pix: usar indice no option value

--- Atividades/atividade-pratica-01/02-pix/javascript/api-load.js
+++ Atividades/atividade-pratica-01/02-pix/javascript/api-load.js
@@ -1,23 +1,25 @@
-function fillSelectBanks(data) {
-    console.log(data);
-    const banks = document.getElementById('banks');
+function fillSelectBanks(data,selectId) {
+    let index = 0;
+    const banks = document.getElementById(selectId);
     clearSelect(banks);
     data.forEach((bank) => {
         const option = document.createElement('option');
-        const { code , name } = bank;
-        option.value = code;
+        const { name } = bank;
+        option.value = index;
         option.innerHTML = name;
         banks.appendChild(option);
+        index += 1;
     });
 }
 
-function loadBanks() {
-    console.log('entrei aqui');
+async function loadBanks() {
     const url = 'https://brasilapi.com.br/api/banks/v1';
-    fetch(url)
+    const data = await fetch(url)
         .then((response) => response.json())
-        .then((data) => fillSelectBanks(data))
         .catch((error) => console.error(error.message));
+
+    fillSelectBanks(data,'sendBank');
+    fillSelectBanks(data,'receiveBank');
 }
 
 function clearSelect(select) {
